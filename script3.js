73ccb1ad6c23e3ea1b124e6696e015d9e3311a19
Urmat Dubanaev <urmatd@gmail.com>
2022-01-28 19:29:14 +0600

    Task-1

---
+++ script3.js
@@ -0,0 +1,8 @@
+/*
+3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+*/
+
+let userName = "Urmat Dubanaev";
+let userEmail = "urmat.dubanaev.pls@gmail.com";
+console.log("Имя: " + userName);
+console.log("Почтовый адрес: " + userEmail);
